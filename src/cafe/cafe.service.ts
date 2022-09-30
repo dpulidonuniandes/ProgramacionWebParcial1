901bf3a6769329fe3afb37499ba235bdc88769da
dpulidonuniandes <69642387+dpulidonuniandes@users.noreply.github.com>
2022-09-30 09:08:16 -0500

    restricciones de negocio en logica

--- src/cafe/cafe.service.ts
+++ src/cafe/cafe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from 'shared/errors/business-errors';
 import { Repository } from 'typeorm';
 import { CafeEntity } from './cafe.entity';
 
@@ -12,7 +13,12 @@ export class CafeService {
     ){}
     
       async createCafe(cafe: CafeEntity): Promise<CafeEntity> {
-        return await this.cafeRepository.save(cafe);
+        const cafeRetorno: CafeEntity = await this.cafeRepository.save(cafe);
+        if (cafe.precio>=0)
+          throw new BusinessLogicException("El cafe tiene que tener un precio mayor a 0", BusinessError.NOT_FOUND);
+
+        return cafeRetorno
+
     }
 
 }
--- src/tienda/tienda.service.ts
+++ src/tienda/tienda.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from 'shared/errors/business-errors';
 import { Repository } from 'typeorm';
 import { TiendaEntity } from './tienda.entity';
 
@@ -12,7 +13,12 @@ export class TiendaService {
       ){}
     
       async createTienda(tienda: TiendaEntity): Promise<TiendaEntity> {
-        return await this.tiendaRepository.save(tienda);
+        const tiendaRetorno: TiendaEntity = await this.tiendaRepository.save(tienda);
+        if (tienda.telefono.length!=10)
+          throw new BusinessLogicException("El cafe tiene que tener un precio mayor a 0", BusinessError.NOT_FOUND);
+
+        return tiendaRetorno
+
     }
 
 }
