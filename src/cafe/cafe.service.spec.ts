84e97655f32bed14022c1657d456a53bd60820b3
dpulidonuniandes <69642387+dpulidonuniandes@users.noreply.github.com>
2022-09-30 09:17:56 -0500

    Pruebas cafe y tienda

--- src/cafe/cafe.service.spec.ts
+++ src/cafe/cafe.service.spec.ts
@@ -1,18 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmTestingConfig } from 'shared/testing-utils/typeorm-testing-config';
+import { Repository } from 'typeorm';
+import { CafeEntity } from './cafe.entity';
 import { CafeService } from './cafe.service';
+import { faker } from '@faker-js/faker';
 
 describe('CafeService', () => {
   let service: CafeService;
-
+  let repository: Repository<CafeEntity>;
+  let cafesList: CafeEntity[];
+ 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CafeService],
-    }).compile();
+   const module: TestingModule = await Test.createTestingModule({
+     imports: [...TypeOrmTestingConfig()],
+     providers: [CafeService],
+   }).compile();
+ 
+   service = module.get<CafeService>(CafeService);
+   repository = module.get<Repository<CafeEntity>>(getRepositoryToken(CafeEntity));
+  await seedDatabase();
+ });
 
-    service = module.get<CafeService>(CafeService);
-  });
+ const seedDatabase = async () => {
+  repository.clear();
+  cafesList = [];
+  for(let i = 0; i < 5; i++){
+      const cafe: CafeEntity = await repository.save({
+        nombre: faker.lorem.sentence(),
+        descripcion: faker.lorem.sentence(),
+        precio: Math.floor(Math.random() * 100),
+      })
+      cafesList.push(cafe);
+  }
+}
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('createCafe should return a new cafe', async () => {
+    const cafe: CafeEntity = {
+      id: Math.floor(Math.random() * 100),
+      nombre: faker.lorem.sentence(),
+      descripcion: faker.lorem.sentence(),
+      precio: Math.floor(Math.random() * 100),
+      tiendas: []
+    }
+
+    const newCafe: CafeEntity = await service.createCafe(cafe);
+  expect(newCafe).not.toBeNull();
+
+  const storedCafe: CafeEntity = await repository.findOne({where: {id: newCafe.id}})
+  expect(cafe).not.toBeNull();
+  expect(cafe.nombre).toEqual(storedCafe.nombre)
+  expect(cafe.descripcion).toEqual(storedCafe.descripcion)
+  expect(cafe.precio).toEqual(storedCafe.precio)
+
+});
 });
--- src/tienda/tienda.service.spec.ts
+++ src/tienda/tienda.service.spec.ts
@@ -1,18 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmTestingConfig } from 'shared/testing-utils/typeorm-testing-config';
+import { Repository } from 'typeorm';
+import { TiendaEntity } from './tienda.entity';
 import { TiendaService } from './tienda.service';
+import { faker } from '@faker-js/faker';
 
 describe('TiendaService', () => {
   let service: TiendaService;
-
+  let repository: Repository<TiendaEntity>;
+  let tiendasList: TiendaEntity[];
+ 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TiendaService],
-    }).compile();
+   const module: TestingModule = await Test.createTestingModule({
+     imports: [...TypeOrmTestingConfig()],
+     providers: [TiendaService],
+   }).compile();
+ 
+   service = module.get<TiendaService>(TiendaService);
+   repository = module.get<Repository<TiendaEntity>>(getRepositoryToken(TiendaEntity));
+  await seedDatabase();
+ });
 
-    service = module.get<TiendaService>(TiendaService);
-  });
+ const seedDatabase = async () => {
+  repository.clear();
+  tiendasList = [];
+  for(let i = 0; i < 5; i++){
+      const tienda: TiendaEntity = await repository.save({
+        nombre: faker.lorem.sentence(),
+        descripcion: faker.lorem.sentence(),
+        precio: Math.floor(Math.random() * 100),
+      })
+      tiendasList.push(tienda);
+  }
+}
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('createTienda should return a new tienda', async () => {
+    const tienda: TiendaEntity = {
+      id: Math.floor(Math.random() * 100),
+      nombre: faker.lorem.sentence(),
+      direccion: faker.lorem.sentence(),
+      telefono: faker.lorem.sentence(),
+      cafes: []
+    }
+
+    const newTienda: TiendaEntity = await service.createTienda(tienda);
+  expect(newTienda).not.toBeNull();
+
+  const storedTienda: TiendaEntity = await repository.findOne({where: {id: newTienda.id}})
+  expect(tienda).not.toBeNull();
+  expect(tienda.nombre).toEqual(storedTienda.nombre)
+  expect(tienda.direccion).toEqual(storedTienda.direccion)
+  expect(tienda.telefono).toEqual(storedTienda.telefono)
+
+});
 });
